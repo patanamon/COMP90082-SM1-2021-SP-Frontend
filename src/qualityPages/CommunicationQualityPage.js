1730627a20e0869ea5a961d626c10315754e5530
YipChiSan <zye39@yahoo.com>
2021-04-23 11:48:36 +1000

    Create some mock data to test table and graph

--- src/qualityPages/CommunicationQualityPage.js
+++ src/qualityPages/CommunicationQualityPage.js
@@ -6,8 +6,39 @@ import { storeGet } from '../_helpers/helper-funcs';
 import uomHeader from '../header/uomheader';
 import {userActions} from '../_actions';
 import { connect } from 'react-redux';
+import { commonConstants } from '../_constants';
 
 class CommunicationPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            teamID : 1,
+
+            confluenceMeeting : [{meetingName : "Frontend 23/04/21 meeting", time : "23/04/21", meetingMinutes: '#' }], 
+
+            confluenceHeader : [
+                {meetingName: [], time : [], meetingMinutes : []}
+            ],
+
+            btnNames: [
+                commonConstants.CONFLUENCE,
+                commonConstants.GITHUB,
+            ],
+
+            githubData : {
+                labels: ["01/03", "10/03", "20/03", "30/03", "09/04", "19/04"],
+                datasets : [
+                    {
+                        label : "Number of Comments",
+                        data : [10, 20, 30, 40, 50, 70]
+                    }
+                ]
+            }
+
+        }
+        this.reFetch = this.reFetch.bind(this);
+    }
+
     render() {
         return(
             <div className="uomcontent">
