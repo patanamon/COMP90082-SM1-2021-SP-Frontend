23b05a42a2f0e8d7ca72ab2c7c8890706e9b7a2b
Saiskye <69886053+Saiskye@users.noreply.github.com>
2021-04-27 22:22:55 +1000

    Update Coordinator Home UI (#13)
    * update HomePage js file
    
    * add id to fake data
    
    * update_home_page_UI_seleting_box
    
    Co-authored-by: Ruofan Zhang <67733681+ruofzhang@users.noreply.github.com>

--- src/CoordinatorPage/ProjectList.js
+++ src/CoordinatorPage/ProjectList.js
@@ -1,9 +1,10 @@
 export const projects = [
-  { id: "001", link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample1", label: "Algorithms in Action" },
-  { id: "002", link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample2", label: "Motivational Modelling" },
-  { id: "003", link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample3", label: "NDIS planners for Capital Guardians" },
-  { id: "004", link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample4", label: "Planimation" },
-  { id: "005", link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample5", label: "ePortfolio" },
-  { id: "006", link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample6", label: "Software Resources for CIS subjects" },
-  { id: "007", link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample7", label: "Software Project Databases" }
+  { link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample1", label: "Algorithms in Action" },
+  { link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample2", label: "Motivational Modelling" },
+  { link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample3", label: "NDIS planners for Capital Guardians" },
+  { link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample4", label: "Planimation" },
+  { link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample5", label: "ePortfolio" },
+  { link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample6", label: "Software Resources for CIS subjects" },
+  { link: "https://confluence.cis.unimelb.edu.au:8443/display/Sample7", label: "Software Project Databases" }
 ];
+
