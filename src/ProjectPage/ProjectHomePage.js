ff0851ec697a6ee2cd523ef4b749ce6dca096a14
yuhxie <yuhxie@student.unimelb.edu.au>
2021-04-29 14:40:53 +0800

    Modify project homepage
    1. Add banner
    2. Amplify the table size

--- src/ProjectPage/ProjectHomePage.js
+++ src/ProjectPage/ProjectHomePage.js
@@ -4,7 +4,7 @@ import uomHeader from "../header/uomheader.js";
 import { storeGet } from "../_helpers/helper-funcs";
 import { userActions } from "../_actions";
 import Table from "../_utils/Table";
-
+import Banner from "../_utils/Banner";
 
 const team = 1;
 // var bigImg = document.createElement("img");     //创建一个img元素
@@ -20,9 +20,16 @@ class ProjectHomePage extends Component {
       teamList: [{ student_id: 0, fullname: "", email: "" }],
       submitted: true,
       data: [
-        { id: 1, student: "Student 1", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004225/64775568-teen-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 123, email_address: "student1@student.unimleb.edu.au"},
-        { id: 2, student: "Student 2", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004227/64775584-teen-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 456, email_address: "student2@student.unimleb.edu.au"},
-        { id: 3, student: "Student 3", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004256/64775431-happy-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 789, email_address: "student3@student.unimleb.edu.au"},
+        { id: 1, student: "Student 1", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004225/64775568-teen-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 123423, email_address: "student1@student.unimleb.edu.au"},
+        { id: 2, student: "Student 2", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004227/64775584-teen-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 423456, email_address: "student2@student.unimleb.edu.au"},
+        { id: 3, student: "Student 3", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004256/64775431-happy-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 234789, email_address: "student3@student.unimleb.edu.au"},
+        { id: 4, student: "Student 4", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004225/64775568-teen-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 122343, email_address: "student4@student.unimleb.edu.au"},
+        { id: 5, student: "Student 5", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004227/64775584-teen-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 623452, email_address: "student5@student.unimleb.edu.au"},
+        { id: 6, student: "Student 6", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004256/64775431-happy-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 343789, email_address: "student6@student.unimleb.edu.au"},
+        { id: 7, student: "Student 7", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004225/64775568-teen-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 89123, email_address: "student7@student.unimleb.edu.au"},
+        { id: 8, student: "Student 8", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004227/64775584-teen-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 983456, email_address: "student8@student.unimleb.edu.au"},
+        { id: 9, student: "Student 9", profile:<img alt='Avatar' width='40' src="https://previews.123rf.com/images/djvstock/djvstock1610/djvstock161004256/64775431-happy-boy-character-avatar-vector-illustration-design.jpg"/>, student_id: 71289, email_address: "student9@student.unimleb.edu.au"},
+ 
       ],
 
       columns: [
@@ -100,9 +107,10 @@ class ProjectHomePage extends Component {
     return (
       <div className="uomcontent">
         {uomHeader("Project Overview")}
-        <div role="main">
-          <div className="page-inner">
-            <Table columns={this.state.columns} data={this.state.data} title={"Student Information"}/>
+        <div role="main" >
+          <div className="page-inner" >
+            <Banner projName="2021-SM1-Software-Project-Database" />
+            <Table columns={this.state.columns} data={this.state.data} title={"Student Information"} width="1000px" height="800px"/>
             {/* <a className="button cta" onClick={this.handleSubmitTeamList} >Get Team List</a>
                         {submitted && <table id='projects' className="zebra" data-sortable="">
                             <h2>Team Member List</h2>
