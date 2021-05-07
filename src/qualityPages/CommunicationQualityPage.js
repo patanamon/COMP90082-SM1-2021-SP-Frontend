01ecd71f8982818f46523024ff0062b3e1f586be
YipChiSan <zye39@yahoo.com>
2021-05-07 11:54:58 +1000

    Delete all functions related to GitHub comments

--- src/qualityPages/CommunicationQualityPage.js
+++ src/qualityPages/CommunicationQualityPage.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import Banner from '../_utils/Banner';
-import LineChart from '../_utils/LineChart'; 
-import ButtonGroup from '../_utils/ButtonGroup';
 import uomHeader from '../header/uomheader';
 import {userActions} from '../_actions';
 import { connect } from 'react-redux';
-import { commonConstants } from '../_constants';
-
 import Table from '../_utils/Table';
 
 class CommunicationPage extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            teamID : 1,
-
-            btnNames: [
-                commonConstants.CONFLUENCE,
-                commonConstants.GITHUB,
-            ],
-
-            btnSelected : commonConstants.CONFLUENCE,
-            
+        this.state = {            
 
             columns: [
                 {
@@ -37,21 +24,7 @@ class CommunicationPage extends React.Component {
             ],
 
         };
-        this.handleBtnGroupClick = this.handleBtnGroupClick.bind(this);
-        
     }
-
-    handleBtnGroupClick(e) {
-        let selected = e.currentTarget.firstChild.innerHTML;
-        if (selected === commonConstants.CONFLUENCE) {
-          this.props.getTeamConfluenceMeeting("COMP900822021SM1SP");
-        } else  {
-          this.props.getTeamGitHubComments("COMP900822021SM1SP");
-        } 
-        this.setState({
-          btnSelected: selected,
-        });
-      }
 
     componentDidMount(){
         this.props.getTeamConfluenceMeeting("COMP900822021SM1SP");
@@ -65,26 +38,10 @@ class CommunicationPage extends React.Component {
 
                 <div role="main">
                     <div className="page-inner">
-                        <Banner projName="2021-SM1-Software-Project-Database" />
-                        <ButtonGroup 
-                            btnNames={this.state.btnNames}
-                            clickHandler={this.handleBtnGroupClick}
-                            selected={this.state.btnSelected}
-                        />
-                            
-                        
-                        {
-                            this.state.btnSelected === commonConstants.GITHUB &&
-                                <LineChart data={this.props.githubData}/>
-                        }
-                        {
-                            this.state.btnSelected === commonConstants.CONFLUENCE &&
-                                <Table columns={this.state.columns} data={this.props.confluenceData} width={"80vw"} title={""}/>
-                        }
-                        
+                        <Banner projName="2021-SM1-Software-Project-Database" />     
+                        <Table columns={this.state.columns} data={this.props.confluenceData} width={"80vw"} title={""}/>
                     </div>
                 </div>
-
             </div>
         )
     }
@@ -94,12 +51,11 @@ function mapState(state) {
     
     return {
       confluenceData: state.user.teamConfluenceMeeting,
-      githubData: state.user.teamGitHubComments,
+      
     };
   }
   
   const actionCreators = {
-    getTeamGitHubComments: userActions.getTeamGitHubComments,
     getTeamConfluenceMeeting: userActions.getTeamConfluenceMeeting,
   };
 
