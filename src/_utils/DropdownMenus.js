a1674781b5b9fb6fef70a74ba923564b1b375f21
YipChiSan <zye39@yahoo.com>
2021-05-10 13:39:42 +1000

    Create a DropdownMeanus class

---
+++ src/_utils/DropdownMenus.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Tab, Col, Row, Container, Form } from "react-bootstrap";
+
+export default function DropdownMenus(props) {
+    const data = props.data;
+    return (
+        <Form inline>
+          <Form.Label
+            className="col-sm-3"
+            htmlFor="inlineFormCustomSelectPref"   
+          >
+            Student     :
+          </Form.Label>
+          <Form.Control
+            as="select"
+            className="col-sm-5"
+            id="inlineFormCustomSelectPref"
+            custom
+            onChange={props.onChange}
+          >
+            {{data}.map((student, index) => (
+              <option key={index} value={index}>{student}</option>
+            ))}
+          </Form.Control>
+        </Form>
+      );
+}
