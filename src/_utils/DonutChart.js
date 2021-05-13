1980b16de2213e57ca1d4c3eed90f1f729ad5ad6
YipChiSan <zye39@yahoo.com>
2021-05-13 15:57:14 +1000

    Display percentage to the donut chart for each part

--- src/_utils/DonutChart.js
+++ src/_utils/DonutChart.js
@@ -25,7 +25,25 @@ export default function DonutChart(props){
   const data = props.data
   return (
       <div style={{ position: "relative", margin: "auto", width: "80vw" }}>
-        <Doughnut data={data} options={{legend: { display: true, position: "right", labels: {fontSize: 25} }}} />
+        <Doughnut data={data} options={{legend: { display: true, position: "right", labels: {fontSize: 25} },
+        tooltips: {
+          callbacks: {
+            label: function(tooltipItem, data) {
+            //get the concerned dataset
+            var dataset = data.datasets[tooltipItem.datasetIndex];
+            //calculate the total of this data set
+            var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+              return previousValue + currentValue;
+            });
+            //get the current items value
+            var currentValue = dataset.data[tooltipItem.index];
+            //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+            var percentage = Math.floor(((currentValue/total) * 100)+0.5);
+
+            return data.labels[tooltipItem.index] + ": " +percentage + "%";
+    }
+  }
+} }} />
       </div>
   )
 }
