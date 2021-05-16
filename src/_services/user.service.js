1f9ac1c75cc32ff2b2d61e4b5820dbf917a85340
sarah <srs-94@hotmail.com>
2021-05-16 16:20:18 +0300

    baseUrl

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -26,8 +26,8 @@ export const userService = {
   SendImportRequest,
 };
 
-const baseUrl = "http://localhost:3200/api/v1";
-//const baseUrl = "http://18.167.74.23:18000/api/v1";
+//const baseUrl = "http://localhost:3200/api/v1";
+const baseUrl = "http://18.167.74.23:18000/api/v1";
 
 function getTeamConfluencePages(teamKey) {
   let url = baseUrl + "/confluence/spaces/" + teamKey + "/page_count";
